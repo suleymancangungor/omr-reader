a8e727167576e87fca713c8f97a1508163f7999d
suleymancangungor <cangungormail@gmail.com>
2025-08-30 12:58:55 +0300

    images added

--- main.py
+++ main.py
@@ -820,7 +820,7 @@ if __name__ == "__main__":
 
     # main()
 
-    image_folder = ""
+    image_folder = "/home/can/Desktop/me/omr/images/grayscale_output"
     output_txt = "results.txt"
 
     supported_formats = (".jpg",".jpeg",".png")
